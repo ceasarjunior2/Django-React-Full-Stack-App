6e1197b1786ff00a3a415d42621e25f41958e626
ceasarjunior2 <ceasarjunior2@gmail.com>
2024-04-19 18:50:34 +0800

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,11 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "../src/constants"
 
+
+const apiURL = "/choreo-apis/djangoreacttutorialm/backend/rest-api-5c6/v1.0"
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiURL
 })
 
 api.interceptors.request.use(
